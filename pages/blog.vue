8701c606af6a70638bed9941771a34b79763481a
Nathan Fishel <n_fishel-v@dcu.org>
2021-01-16 03:33:50 -0500

    stated post component

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,78 +1,60 @@
 <template>
   <section>
-   <script src="https://kit.fontawesome.com/4faac6e63e.js" crossorigin="anonymous"></script>
-   <section>
+  <script src="https://kit.fontawesome.com/4faac6e63e.js" crossorigin="anonymous"></script>
+  <section>
       <div class="columns">
         <div  class="column main-text">
           <h1>Welcome to my Blog</h1>
       </div>
       </div>
-   </section>
+  </section>
 
-   <section>
-       <div class="columns">
-           <div class="column">
-                <div id="body">
-                    <article class="media" v-for="(posts, index) in blog" :key="index">
-                        <div class="media-content">
-                            <div class="content" >
-                      
-                            <h3>{{ posts.title }}</h3>
-                              
-                            <p>{{ posts.content }}</p>
-                            </div>
-                            <nav class="level is-mobile">
-                            <div class="level-left">
-                                <a class="level-item" :href='posts.link'>
-                                  <p>Read more</p>
-                                </a>
-                            </div>
-                            </nav>
-                        </div>
-                    </article>
-                </div>
-            </div> 
-       </div>
-   </section>
-   
+  <section>
+    <template v-for="post in posts">
+      <div :key="post.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
+        <post :recipe="post"></post>
+      </div>
+    </template>
+  </section>
+
   <div style="padding-bottom: 100px;"></div>
-
-
-
-
   </section> 
 </template>
 
 
 <script>
+import Post from "~/components/Post.vue";
+
+const sampleData = [
+  {
+    id: 1,
+    title: "Jollof Rice",
+    picture: "assets/css/bridge.jpg",
+    category: "Cloud"
+  }
+];
+
 export default {
   layout: "navbar",
+  head() {
+    return {
+      title: "Recipes list"
+    };
+  },
+  components: {
+    Post
+  },
+  asyncData(context) {
+    let data = sampleData;
+    return {
+      posts: data
+    };
+  },
   data(){
     return{
-      blog: [
-        { title: '10 Reasons to not use a Website builder and go with a custom solution', 
-        content: 'Website builders have become widely used in the last few years, by a website builder I mean Wix, Square Space, Weebly. These types of sites do have some benefits. They are cheap and can be set up quickly but a user with no coding knowledge... ',
-        link: '/article4'},
-
-        { title: 'Why your website needs to be fast & how to speed it up', 
-        content: 'Weather your website serves strictly as marketing material or it is your main platform for business, you need to have a fast website. Most people only have an attention span of 8-9 seconds, if users have to wait for your site to load any longer than 1-2 seconds they will leave. ',
-        link: '/article3'},
-        
-         { title: 'Why you should be using Django for your next web project', 
-        content: 'Finding the right tools for a project can sometimes be harder than the actual project itself. In this blog post I will discuss Django, a tool that I think everyone should try for their next web-based project...',
-        link: '/article2'},
-
-         { title: 'What is Google BigQuery ', 
-        content: 'Have you ever spent hours waiting for a SQL query to finish running? Google has a solution for this. Google BigQuery is a data warehousing tool that enables scalable, cost-effective and fast analysis over petabytes of data...',
-        link: '/article1'},
-
-        { title: 'Next Post coming soon', 
-        content: '',
-        link: '/'}
-      ]
+      posts: []
     }
   },
-
 head: {
   meta: [
     { charset: 'utf-8' },
